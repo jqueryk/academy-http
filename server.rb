47a42b01c47b7249e18d7bd6316ceb3a6064b17b
Ilya K. <jqueryk@icloud.com>
2024-02-08 20:35:16 +0300

    Added paths.

--- server.rb
+++ server.rb
@@ -4,11 +4,24 @@ server = TCPServer.open('0.0.0.0', 3000)
 
 while connection = server.accept
   request = connection.gets
+  method, full_path = request.split(' ')
+  path = full_path.split('?')[0]
 
-  connection.print "HTTP/1.1 200\r\n"
+  response = case path
+             when "/"
+               "Hello World"
+             when "/calculator"
+               "Calculator"
+             else
+               "404"
+             end
+
+  status = response != "404" ? 200 : 404
+
+  connection.print "HTTP/1.1 #{status}\r\n"
   connection.print "Content-Type: text/html\r\n"
   connection.print "\r\n"
-  connection.write "Hello World!"
+  connection.print "<h1>#{response}</h1>"
 
   connection.close
 end
